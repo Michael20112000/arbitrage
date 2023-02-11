a0abdfcadb16df14f2755ddf5a7814baa8dcc286
Michael20112000 <official.michael.developer@gmail.com>
2023-02-11 23:30:42 +0200

    checkpoint

--- src/2_Detector/index.js
+++ src/2_Detector/index.js
@@ -4,36 +4,11 @@ export const Detector = new class {
   detectArbitrage({symbolsData, currency, balances, steps}) {
     const currencyBalance = this._getBalance(balances, currency)
 
-    let step1 = this._getTradedSymbols(symbolsData, currency).slice(0, 2)
+    const currencies = this._getCurrencies(symbolsData)
 
-    let step2 = []
+    const currenciesTradesInfo = this._findMentions(currencies, symbolsData)
 
-    let step3 = []
-
-    for (const symb of step1) {
-      const nextCurrency = symb.baseAsset === currency ? symb.quoteAsset : symb.baseAsset
-      step2.push(this._getTradedSymbols(symbolsData, nextCurrency))
-
-      step2.forEach(symbArr => {
-        return symbArr.forEach(symbol => {
-          const nextCurr = symbol.baseAsset === nextCurrency ? symbol.quoteAsset : symbol.baseAsset
-
-          step3.push(this._getTradedSymbols(symbolsData, nextCurr).filter(s => s.baseAsset === currency || s.quoteAsset === currency))
-        })
-      })
-    }
-
-    fs.writeFile('staticData/step1.json', JSON.stringify(step1), err => {
-      if (err) throw err
-      console.log('Data written to file')
-    })
-
-    fs.writeFile('staticData/step2.json', JSON.stringify(step2), err => {
-      if (err) throw err
-      console.log('Data written to file')
-    })
-
-    fs.writeFile('staticData/step3.json', JSON.stringify(step3), err => {
+    fs.writeFile('staticData/currenciesTradesInfo.json', JSON.stringify(currenciesTradesInfo), err => {
       if (err) throw err
       console.log('Data written to file')
     })
@@ -47,8 +22,25 @@ export const Detector = new class {
     }
   }
 
-  _getTradedSymbols(symbolsData, currency) {
-    return symbolsData.filter(symb => symb.baseAsset === currency || symb.quoteAsset === currency)
+  _getCurrencies(symbolsData) {
+    const currencies = new Set()
+
+    symbolsData.forEach(i => {
+      currencies.add(i.baseAsset)
+      currencies.add(i.quoteAsset)
+    })
+
+    return Array.from(currencies)
+  }
+
+  _findMentions(currencies, symbolsData) {
+    const result = {}
+
+    currencies.forEach(currency => {
+      result[currency] = symbolsData.filter(item => item.baseAsset === currency || item.quoteAsset === currency)
+    })
+
+    return result
   }
 }
 
