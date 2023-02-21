0f87c1bc3e8581b17bd946b8b270d0e8e4c9f5be
Michael20112000 <official.michael.developer@gmail.com>
2023-02-21 13:26:52 +0200

    checkpoint

--- src/1_Getter/exchanges/Binance/index.js
+++ src/1_Getter/exchanges/Binance/index.js
@@ -13,7 +13,7 @@ export const Binance = new class {
 
     const {symbols} = exchangeInfo
 
-    let aliveSymbols = Transformer.filterAliveSymbols(symbols).slice(0, 1000)
+    let aliveSymbols = Transformer.filterAliveSymbols(symbols).slice(0, 400)
 
     aliveSymbols = Transformer.trimUnnecessaryInfo(aliveSymbols)
 
--- src/2_Detector/index.js
+++ src/2_Detector/index.js
@@ -8,20 +8,14 @@ export const Detector = new class {
 
     const currenciesTradesInfo = this._findMentions(currencies, symbolsData)
 
-    const chains = this._generateChains({
-      currenciesTradesInfo,
-      target,
-      targetBalance,
-      steps
+    const tree = this._generateTree({
+      currenciesTradesInfo, target, targetBalance, steps
     })
 
-    const queue = this._generateQueue(chains)
+    const queue = this._getAllBranches(tree)
 
     return {
-      target,
-      targetBalance,
-      steps,
-      queue
+      target, targetBalance, steps, queue
     }
   }
 
@@ -51,7 +45,7 @@ export const Detector = new class {
     return result
   }
 
-  _generateChains = ({currenciesTradesInfo, target, targetBalance, steps}) => {
+  _generateTree = ({currenciesTradesInfo, target, targetBalance, steps}) => {
     if (steps !== 0) {
       return currenciesTradesInfo[target].map(variant => {
         const isTargetBaseAsset = variant.baseAsset === target
@@ -60,11 +54,8 @@ export const Detector = new class {
         const type = isTargetBaseAsset ? 'sell' : 'buy'
         const theoreticalQuantity = type === 'buy' ? targetBalance / variant.price : targetBalance * variant.price
 
-        let next = this._generateChains({
-          currenciesTradesInfo,
-          target: nextTarget,
-          targetBalance: theoreticalQuantity,
-          steps: steps - 1
+        let next = this._generateTree({
+          currenciesTradesInfo, target: nextTarget, targetBalance: theoreticalQuantity, steps: steps - 1
         })
 
         if (steps === 2) {
@@ -72,16 +63,107 @@ export const Detector = new class {
         }
 
         return {
-          ...variant,
-          type,
-          theoreticalQuantity,
-          next
+          ...variant, type, theoreticalQuantity, next
         }
       })
     }
   }
 
-  _generateQueue(chains) {
-    return 42
+  _getAllBranches(tree) {
+    const result = []
+
+    tree.forEach(node => {
+      if (node.hasOwnProperty('next')) {
+        result.push([node.symbol, this._getAllBranches(node.next)])
+      } else {
+        result.push([node.symbol])
+      }
+    })
+
+    return result
   }
 }
+
+let first = [
+  {
+    'symbol': 'a',
+    'next': [
+      {
+        'symbol': 'a1',
+        'next': [
+          {
+            'symbol': 'a1.1',
+          },
+          {
+            'symbol': 'a1.2',
+          },
+          {
+            'symbol': 'a1.3',
+          }
+        ]
+      },
+      {
+        'symbol': 'a2',
+        'next': [
+          {
+            'symbol': 'a2.1',
+          },
+          {
+            'symbol': 'a2.2',
+          },
+          {
+            'symbol': 'a2.3',
+          }
+        ]
+      },
+    ]
+  },
+  {
+    'symbol': 'b',
+    'next': [
+      {
+        'symbol': 'b1',
+        'next': [
+          {
+            'symbol': 'b1.1',
+          },
+          {
+            'symbol': 'b1.2',
+          },
+          {
+            'symbol': 'b1.3',
+          }
+        ]
+      },
+      {
+        'symbol': 'b2',
+        'next': [
+          {
+            'symbol': 'b2.1',
+          },
+          {
+            'symbol': 'b2.2',
+          },
+          {
+            'symbol': 'b2.3',
+          }
+        ]
+      },
+    ]
+  },
+]
+
+let second = [
+  [{'symbol': 'a'}, {'symbol': 'a1'}, {'symbol': 'a1.1'}],
+  [{'symbol': 'a'}, {'symbol': 'a1'}, {'symbol': 'a1.2'}],
+  [{'symbol': 'a'}, {'symbol': 'a1'}, {'symbol': 'a1.3'}],
+  [{'symbol': 'a'}, {'symbol': 'a2'}, {'symbol': 'a2.1'}],
+  [{'symbol': 'a'}, {'symbol': 'a2'}, {'symbol': 'a2.2'}],
+  [{'symbol': 'a'}, {'symbol': 'a2'}, {'symbol': 'a2.3'}],
+  [{'symbol': 'b'}, {'symbol': 'b1'}, {'symbol': 'b1.1'}],
+  [{'symbol': 'b'}, {'symbol': 'b1'}, {'symbol': 'b1.2'}],
+  [{'symbol': 'b'}, {'symbol': 'b1'}, {'symbol': 'b1.3'}],
+  [{'symbol': 'b'}, {'symbol': 'b2'}, {'symbol': 'b2.1'}],
+  [{'symbol': 'b'}, {'symbol': 'b2'}, {'symbol': 'b2.2'}],
+  [{'symbol': 'b'}, {'symbol': 'b2'}, {'symbol': 'b2.3'}],
+]
